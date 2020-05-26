9cd89e758ee8d607e986821c215df658a64200aa
Abhishek <abhranja@gmail.com>
2020-05-26 22:35:05 +0530

    Business models

---
+++ src/main/java/com/ar50645/polls/payload/UserIdentityAvailability.java
@@ -0,0 +1,17 @@
+package com.ar50645.polls.payload;
+
+public class UserIdentityAvailability {
+    private Boolean available;
+
+    public UserIdentityAvailability(Boolean available) {
+        this.available = available;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+}
